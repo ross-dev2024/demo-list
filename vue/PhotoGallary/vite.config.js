5feaddd62ad0872808aed9ff5058a84d4e8b5765
wangjw.dev <wangjw.dev@gmail.com>
2025-05-07 14:14:38 +0900

    add public path

--- vue/PhotoGallary/vite.config.js
+++ vue/PhotoGallary/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig(({ command }) => ({
+  base: '/gallery/',
   plugins: [vue()],
   server: command === 'serve' ? {
     host: '0.0.0.0',
